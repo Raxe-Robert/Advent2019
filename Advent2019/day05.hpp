1c1bffc63dc9031623246190ee714704c6bc97af
Robert Bijl <robertbijl@live.nl>
2020-01-17 12:12:57 +0100

    Solved day04 part1

--- Advent2019/day05.hpp
+++ Advent2019/day05.hpp
@@ -2,10 +2,6 @@ void day05(string input)
 {
 	intcodeArr arr = ReadIntcodeInput(input);
 
-	// "before running the program, replace position 1 with the value 12 and replace position 2 with the value 2."
-	arr[1] = 12;
-	arr[2] = 2;
-
 	auto val = IntcodeComputer(arr);
 	printf("[Day02][1] %i\n", val);
 
--- Advent2019/shared/intcode.hpp
+++ Advent2019/shared/intcode.hpp
@@ -1,3 +1,18 @@
+constexpr s32 UNICODE_NUMBER_OFFSET = 48;
+
+enum instruction_type : s32 {
+	add = 1,
+	multiply = 2,
+	user_input = 3,
+	output = 4,
+	terminate_program = 99
+};
+
+enum parameter_mode : s32 {
+	position = 0,
+	immediate = 1
+};
+
 inline
 s32 IntcodeComputer(const intcodeArr inputArr)
 {
@@ -8,53 +23,120 @@ s32 IntcodeComputer(const intcodeArr inputArr)
 	memset(arr.Data, 0, sizeof(s32) * arr.Length);
 	memcpy(arr.Data, inputArr.Data, sizeof(s32) * inputArr.Length);
 
+	char instruction[(instruction_type)64];
+	instruction_type opcode;
+	s32 parameterModes[(parameter_mode)64];
+
 	for (auto i = 0; i < arr.Length;)
 	{
-		char numberstring[(sizeof(arr[i]) * CHAR_BIT + 2) / 3 + 2];
-		sprintf(numberstring, "%i", arr[i]);
+		// Read instruction
+		sprintf(instruction, "%i", arr[i]);
+		s32 instruction_length = 0;
 
-		// todo, use lines above
-		auto opcode = arr[i];
+		while (instruction[instruction_length] != '\0')
+			instruction_length++;		
 
-		// Opcode is the rightmost two digits of the first value in an instruction
-		// Parameter modes are single digits, one per paramter, read right-to-left from the opcode
-		//		The first paramter's mode is in the hundreds digit
-		//		The second parameter's mode is in the the thousands digit
-		//		Etc... Any missing modes are 0!
-		//		
+		if (instruction_length <= 0)
+			throw std::length_error("invalid instruction");
+		else if (instruction_length == 1)
+			opcode = instruction_type(instruction[0] - UNICODE_NUMBER_OFFSET);
+		else
+		{
+			auto left = (instruction[instruction_length - 2] - UNICODE_NUMBER_OFFSET) * 10;
+			auto right = instruction[instruction_length - 1] - UNICODE_NUMBER_OFFSET;
+			opcode = instruction_type(left + right);
+		}
 
-		auto valPos = arr[i + 1];
-		auto valPos2 = arr[i + 2];
-		auto outputPos = arr[i + 3];
+		s32 parameterModes_length = instruction_length > 2 ? instruction_length - 2 : 0;
+		for (s32 j = 0; j < parameterModes_length; ++j)
+		{
+			auto val = instruction[(parameterModes_length - 1) - j];
+			parameterModes[j] = parameter_mode(val - UNICODE_NUMBER_OFFSET);
+		}
 
-		auto val1 = arr[valPos];
-		auto val2 = arr[valPos2];
+		//printf("instruction length: %i\n", instruction_length);
+		//printf("opcode: %i\n", opcode);
+
+		printf(">>> %s = opcode: %i\n", instruction, opcode);
+
+		printf("[modes]");
+		for (s32 j = 0; j < parameterModes_length; ++j)
+			printf(" %i", parameterModes[j]);
+		printf("\n");
 
 		switch (opcode)
 		{
-			case 99:
+			case instruction_type::terminate_program:
 				return arr[0];
-			case 1:
+			case instruction_type::add:
+			{
+				s32 parameter0 = arr[i + 1];
+				s32 parameter1 = arr[i + 2];
+				s32 parameter2 = arr[i + 3]; // Parameters that an instruction writes to will never be in imediate mode
+				printf("[param] %i, %i, %i\n", parameter0, parameter1, parameter2);
 
-				arr[outputPos] = val1 + val2;
+				auto val0 = parameterModes[0] == position ? arr[parameter0] : parameter0;
+				auto val1 = parameterModes[1] == position ? arr[parameter1] : parameter1;
+
+				printf("[wrtng] val: '%i' to position: '%i'\n", val0 + val1, parameter2);
+				arr[parameter2] = val0 + val1;
 
 				i += 4;
 				break;
-			case 2:
+			}
+			case instruction_type::multiply:
+			{
+				s32 parameter0 = arr[i + 1];
+				s32 parameter1 = arr[i + 2];
+				s32 parameter2 = arr[i + 3]; // Parameters that an instruction writes to will never be in imediate mode
+				printf("[param] %i, %i, %i\n", parameter0, parameter1, parameter2);
 
-				arr[outputPos] = val1 * val2;
+				auto val0 = parameterModes[0] == position ? arr[parameter0] : parameter0;
+				auto val1 = parameterModes[1] == position ? arr[parameter1] : parameter1;
+
+				printf("[wrtng] val: '%i' to position: '%i'\n", val0 * val1, parameter2);
+				arr[parameter2] = val0 * val1;
 
 				i += 4;
 				break;
-			case 3:
+			}
+			case instruction_type::user_input:
+			{
+				s32 parameter0 = arr[i + 1]; // Parameters that an instruction writes to will never be in imediate mode
+				printf("[param] %i\n", parameter0);
+
+				s32 inputVal;
+				printf("[input] Enter ID: ");
+				scanf("%d", &inputVal);
+
+				printf("[wrtng] val: '%i' to position: '%i'\n", inputVal, parameter0);
+				arr[parameter0] = inputVal;
+
+				i += 2;
+				break;
+			}
+			case instruction_type::output:
+			{
+				s32 parameter0 = arr[i + 1];
+				printf("[param] %i\n", parameter0);
+
+				s32 val0 = parameterModes[0] == position ? arr[parameter0] : parameter0;
+
+				printf("[outpt] %i\n", val0);
 
 				i += 2; //?
 				break;
-			case 4:
-
-				i += 2; //?
-				break;
+			}
+			default:
+			{
+				printf("[Excep] Opcode %i does not exist", opcode);
+				return -1;
+			}
 		}
+		printf("\n");
+
+		// Reset memory
+		memset(parameterModes, 0, (s32)64);
 	}
 }
 
